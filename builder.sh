f1c76d89a7b8f57220527b9331e8f8f82be3576d
ZoeFiri <ZoeFiri@github.com>
2019-05-05 01:42:54 -0600

    f

--- builder.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-read -r header < "posts/1 day1/header"
-read -r content < "posts/1 day1/content"
-read -r title < "posts/1 day1/title"
-sed -e "s/#header/$header/" -e "s/#content/$content/" -e "s/#title/$title/" templates/fuck.html > fuck.html
---
+++ posts/builder.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+i=1
+for dir in orig/*
+do
+    read -r header < "$dir/header"
+    read -r content < "$dir/content"
+    read -r title < "$dir/title"
+    filename=$(basename $dir)
+    sed -e "s/#header/$header/" -e "s/#content/$content/" -e "s/#title/$title/" ../templates/fuck.html > html/$filename.html
+    pagebutton="<button onclick=\"window.location.href = '$filename.html'\" class=\"pagebutton$i\">$header<\/button>"
+    pages="$pages\n$pagebutton"
+    i=$((i+1))
+    [ $i = 7 ] && i=1
+done
+sed -e "s/#posts/$pages/g" ../templates/posts.html > ../pages/posts.html
+cp html/* ../pages/
+git add -A
+echo post info
+read commitmessage
+git commit -m "$commitmessage"
+git push
